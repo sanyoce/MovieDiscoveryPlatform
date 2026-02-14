c0c6eafd423b159825342693d77e1b897fde7f03
Aleksandr Yugay <yugaysasha@icloud.com>
2026-02-14 22:33:56 +0500

    added a home page and did mobile-burger menu

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,30 +1,38 @@
 export function Header(){
     const header = document.createElement('header')
     header.innerHTML = `
-    <div class = 'headerLogo'>
-        <a>
-            <img src='./assets/icons/Logo.svg' alt="logo"/>
-        </a>
-    </div>
+    <div class = 'headerWrapper'>
 
-    <div class = 'headerLinks'>
-            <a href="#/home" class = 'navLinks'>HOME</a>
+        <div class = 'headerLogo'>
+            <a>
+                <img src='./assets/icons/Logo.svg' alt="logo"/>
+            </a>
+        </div>
+
+        <div class = 'headerLinks'>
+            <a href="#/home" class = 'navLinks headerLinksClick'>HOME</a>
             <a href="#/search" class = 'navLinks'>SEARCH</a>
             <a href="#/favorites" class = 'navLinks'>FAVORITES</a>
             <a href="#/details" class = 'navLinks'>DETAILS</a>
-    </div>
-
-    <div class = 'navButtons'>
-        <div class = 'buttonSearch'>
-            <img src='./assets/icons/Vector.svg' alt="#"/>
         </div>
 
-        <div class = 'buttonNotification'>
-            <img src='./assets/icons/Vector (1).svg' alt="#"/>
-        </div>
-    </div>
+        <div class = 'headerButtons'>
 
-    <div class='burger'>☰</div>
+            <div class = 'buttonSearch'>
+                <img src='./assets/icons/Vector.svg' alt="#"/>
+            </div>
+
+            <div class = 'buttonNotification'>
+                <img src='./assets/icons/Vector (1).svg' alt="#"/>
+            </div>
+
+        </div>
+
+        <div class='burger'>
+            <span></span><span></span><span></span>
+        </div>
+
+    </div>
     `;
     return header
 }   
@@ -41,4 +49,16 @@ document.addEventListener('DOMContentLoaded', () => {
             link.classList.add('headerLinksClick')
         })
     })
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.headerWrapper')
+    
+    const burger = document.querySelector('.burger')
+
+    burger.addEventListener('click', () =>{
+        menu.classList.toggle('open')
+    })
+        
+    
 });
--- src/js/pages/HomePage.js
+++ src/js/pages/HomePage.js
@@ -1,7 +1,25 @@
 export function homePage(){
     const div = document.createElement('div')
-    const namePage = document.createElement('h1')
-    namePage.textContent = 'This is the movie platform'
-    div.append(namePage)
+    div.classList.add('homePage')
+    div.innerHTML = `
+        <div>
+            <div><img src='./assets/images/Abstract Design (1).svg'/></div>
+        </div>
+
+        <div class = 'homeAbout'>
+
+            <H1>The Best Streaming Experience</H1>
+
+            <p class = 'homeAboutTxt'>StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With StreamVibe, you can enjoy a wide variety of content, including the latest blockbusters, classic movies, popular TV shows, and more. You can also create your own watchlists, so you can easily find the content you want to watch.</p>
+
+            <p class = 'homeAboutPhone'>StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere.</p>
+
+            <div class = 'homeAboutBtn'>
+                <img src='./assets/icons/Icon.svg'>
+                <p>Start Watching Now</p>
+            </div>
+
+        </div>
+    `
     return div
 }
