9030203c69c648d445a8a8b57a00934bc71f9184
Aleksandr Yugay <yugaysasha@icloud.com>
2026-02-18 18:29:00 +0500

    добавил еще один элемент на homePage

--- src/js/pages/HomePage.js
+++ src/js/pages/HomePage.js
@@ -5,9 +5,9 @@ export async function homePage(){
     const div = document.createElement('div')
     div.classList.add('homePage')
     div.innerHTML = `
-        <div>
-            <div><img src='./assets/images/Abstract Design (1).svg'/></div>
-        </div>
+
+        <div class = 'homeImg'><img src='./assets/images/Abstract Design (1).svg'/></div>
+        
 
         <div class = 'homeAbout'>
 
@@ -24,58 +24,137 @@ export async function homePage(){
 
         </div>
     `;
-    const IMG_BASE = 'https://image.tmdb.org/t/p/w200';
+const IMG_BASE = 'https://image.tmdb.org/t/p/w200';
 
-    const genresData = await getGenres()
-    const postersData = await getPoster()
+const genresData = await getGenres()
+const postersData = await getPoster()
 
 
-    const genresSectionPage = document.createElement('section')
-    genresSectionPage.classList.add('genresPage')
-    genresSectionPage.innerHTML =`
+const genresSectionPage = document.createElement('section')
+genresSectionPage.classList.add('genresPage')
+genresSectionPage.innerHTML =`
+`;
+
+
+const genres = genresData.genres.slice(0, 5);
+const movies = postersData.results;
+
+
+genres.forEach(genre => {
+
+    const genreBlock = document.createElement('div')
+    genreBlock.classList.add('genreBlock')
+
+    genreBlock.innerHTML = `
+        <div class="posters"></div>
+
+        <div class = 'genreTxt'>
+            <h3>${genre.name}</h3>
+            <img src = './assets/icons/Vector 619.svg'>
+        </div>
     `;
 
+    const genrePoster = genreBlock.querySelector('.posters')
 
-    const genres = genresData.genres.slice(0, 4);
-    const movies = postersData.results;
+    const moviesGenre = movies
+        .filter(movie => movie.genre_ids?.includes(genre.id))
+        .slice(0, 4)
 
+    moviesGenre.forEach(movie => {
+        if (!movie.poster_path) return
 
-    genres.forEach(genre => {
+        const img = document.createElement('img')
+        img.src = IMG_BASE + movie.poster_path
+        img.alt = movie
+        genrePoster.append(img)
+    });
 
-        const genreBlock = document.createElement('div')
-        genreBlock.classList.add('genreBlock')
-
-        genreBlock.innerHTML = `
-            <div class="posters"></div>
-
-            <div class = 'genreTxt'>
-                <h3>${genre.name}</h3>
-                <img src = './assets/icons/Vector 619.svg'>
-            </div>
-        `;
-
-        const genrePoster = genreBlock.querySelector('.posters')
-
-        const moviesGenre = movies
-            .filter(movie => movie.genre_ids?.includes(genre.id))
-            .slice(0, 4)
-
-        moviesGenre.forEach(movie => {
-            if (!movie.poster_path) return
-
-            const img = document.createElement('img')
-            img.src = IMG_BASE + movie.poster_path
-            img.alt = movie
-            genrePoster.append(img)
-        });
-
-        genresSectionPage.append(genreBlock);
+genresSectionPage.append(genreBlock);
 });
 
 
+const deviceSection = document.createElement('div')
+deviceSection.classList.add('deviceSectionBlock')
+deviceSection.innerHTML = `
+    <div class = 'deviceSectionInfo'>
+        <h2>We Provide you streaming experience across various devices.</h2>
+        <p class = 'deviceNotHiddenInfo'>With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.</p>
+        <p class = 'deviceHiddenInfo'>With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere.</p>
+    </div>
 
+    <div class = 'deviceSectionCards'>
+
+        <div class = 'deviceSectionCard'>
+            <div>
+                <div class = 'deviceIcons'><img src = './assets/icons/Frame.svg'></div>
+                <h3>Smartphones</h3>
+            </div>
+
+            <div>
+                <p>StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+            </div>
+        </div>
+
+        <div class = 'deviceSectionCard'>
+            <div>
+                <div class = 'deviceIcons'><img src = './assets/icons/Frame (1).svg'></div>
+                <h3>Tablet</h3>
+            </div>
+
+            <div>
+                <p>StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+            </div>
+        </div>
+
+        <div class = 'deviceSectionCard'>
+            <div>
+                <div class = 'deviceIcons'><img src = './assets/icons/Frame (2).svg'></div>
+                <h3>Smart</h3>
+            </div>
+
+            <div>
+                <p>StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+            </div>
+        </div>
+
+        <div class = 'deviceSectionCard'>
+            <div>
+                <div class = 'deviceIcons'><img src = './assets/icons/Frame (3).svg'></div>
+                <h3>Laptops </h3>
+            </div>
+
+            <div>
+                <p>StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+            </div>
+        </div>
+
+        <div class = 'deviceSectionCard'>
+            <div>
+                <div class = 'deviceIcons'><img src = './assets/icons/Frame (4).svg'></div>
+                <h3>Gaming Consoles</h3>
+            </div>
+
+            <div>
+                <p>StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+            </div>
+        </div>
+
+        <div class = 'deviceSectionCard'>
+            <div>
+                <div class = 'deviceIcons'><img src = './assets/icons/Frame (5).svg'></div>
+                <h3>VR Headsets </h3>
+            </div>
+
+            <div>
+                <p>StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+            </div>
+        </div>
+
+    </div>
+`
 
 div.append(genresSectionPage)
+div.append(deviceSection)
 return div
 }
 
